6338b02e419c0fc95cd53295824e09f9cf6777ba
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-06-27 15:50:24 +0300

    finished the main section

--- infosite/solo_project/src/components/App.js
+++ infosite/solo_project/src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import TopSection from "./TopSection";
+import MainSecion from "./MainSecion";
 
 function App() {
   return (
     <div className="App">
       <TopSection />
+      <MainSecion />
     </div>
   );
 }
---
+++ infosite/solo_project/src/components/MainSecion.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./MainSection.css";
+
+export default function MainSecion() {
+  return (
+    <div className="main--main">
+      <h4 className="main--title">About</h4>
+      <p className="main--p">
+        I am a frontend developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
+      </p>
+      <h4 className="main--title">Interests</h4>
+      <p className="main--p">
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  );
+}
